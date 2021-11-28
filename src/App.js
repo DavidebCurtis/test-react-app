0515cb524eef59860877eb2bad7c11f684628487
david curtis <heythereimdavidcurtis@gmail.com>
2021-11-28 14:34:24 -0500

    Added Modals and Backdrop. Implemented onClick functions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 
 
-function App() {
-  return  <div>
-      <h1>My Todos</h1>
-      <div>
-        <h2>TITLE</h2>
-        <div>
-          <button>Delete</button> 
-        </div>
+import Todo from "./Components/Todo";
 
-      </div>
-  </div>
-   
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React"/>
+      <Todo text="Explore React"/>
+      <Todo text="Master React"/>
+
+    </div>
+  );
 }
 
 export default App;
